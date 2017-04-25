c31ce703a9e9767f7d820f4c8d2fa26a6936aa3c
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-04-25 02:10:24 -0500

    Changed file extensions to match their language.

--- largestPalindrome/javascript.js
+++ largestPalindrome/javascript.js
@@ -1,11 +1,11 @@
-Question:
-
-A palindromic number reads the same both ways.
-The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
-Find the largest palindrome made from the product of two 3-digit numbers.
-
-Answer:
+// Question:
+//
+// A palindromic number reads the same both ways.
+// The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//
+// Find the largest palindrome made from the product of two 3-digit numbers.
+//
+// Answer:
 
 function largestPalindrome() {
   var palindrome = 0;
--- multiplesOf3And5/php.php
+++ multiplesOf3And5/php.php
@@ -5,7 +5,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 
 Answer:
-
+<?php
 function sumOfMultiples($val1, $val2, $range) {
   $sum = 0;
   $currentNumber = 1;
